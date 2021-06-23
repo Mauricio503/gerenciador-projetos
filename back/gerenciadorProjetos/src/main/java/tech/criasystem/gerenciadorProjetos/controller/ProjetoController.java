d438d9a454f7c1b34f8f470300eff52fdda1e1ce
Maurício Spagnol <mauricio@faee.com.br>
2021-06-23 19:49:29 -0300

    visualização projetos

--- back/gerenciadorProjetos/src/main/java/tech/criasystem/gerenciadorProjetos/controller/ProjetoController.java
+++ back/gerenciadorProjetos/src/main/java/tech/criasystem/gerenciadorProjetos/controller/ProjetoController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -39,7 +38,8 @@ public class ProjetoController {
 	}
 	
 	@RequestMapping(value="/salvarProjeto", method=RequestMethod.POST)
-	public String salvarProjeto(Projeto projeto, BindingResult result, RedirectAttributes attributes) {
+	public String salvarProjeto(Projeto projeto, BindingResult result,
+			RedirectAttributes attributes) {
 		projetoService.salvar(projeto);
 		return "redirect:/";
 	}
